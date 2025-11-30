e2fb850f3ba63af903956607053d1dc7300d12b0
Gavin Payne <gavinapayne@gmail.com>
2025-11-30 13:05:56 +0400

    v1.1.0

--- src/GeminiImageGen/node/index.ts
+++ src/GeminiImageGen/node/index.ts
@@ -11,7 +11,7 @@ export function createNodeDefinition(): EnhancedNodeDefinition {
   const { NodeInputType } = getPlatformDependencies();
 
   return {
-    packageVersion: "1.0.1",
+    packageVersion: "1.0.2",
     type: "GeminiImageGen",
     isService: false,
     name: "Gemini Image Gen",
